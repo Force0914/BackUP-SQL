0a114062c88300ab1f965ea418b21226b9ef565e
Force0914 <45906848+Force0914@users.noreply.github.com>
2021-03-07 15:21:51 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const config = require(`${process.cwd()}/config.json`);
 const state = config.autodelete;
 console.log("------------------------------------------\nServer Mysql Back UP\nVer 2.0.0\nPower By SmallDevil\n------------------------------------------\n");
 backup();
-setInterval(() => {
-    backup();
+setInterval(async () => {
+    await backup();
 }, 3600000);
-function backup() {
+async function backup() {
         var today=new Date();
         var month = today.getMonth()+1
         var date = today.getDate()
@@ -15,12 +15,12 @@ function backup() {
         var min = today.getMinutes()
         var time = `${today.getFullYear()}${padStart(month)}${padStart(date)}_${padStart(hour)}${padStart(min)}`
         if(state){
-        console.log("Start Remove A Days Ago File\n")
-        cp.exec(`forfiles /p "db_backup" /d -1 /s /m *.* /c "cmd /c del @path"`)
-        console.log("Remove Done\n")
+        await console.log("Start Remove A Days Ago File\n")
+        await cp.exec(`forfiles /p "db_backup" /d -1 /s /m *.* /c "cmd /c del @path"`)
+        await console.log("Remove Done\n")
         }
-        console.log("Start BackUP SQL\n")
-        mysqldump({
+        await console.log("Start BackUP SQL\n")
+        await mysqldump({
             connection: {
                 host: config.host,
                 user: config.dbaccount,
@@ -29,8 +29,8 @@ function backup() {
             },
             dumpToFile: `${process.cwd()}/db_backup/${time}.sql`,
         });
-        console.log(`BackUP Done(${time}.sql)\n`)
-        console.log(`Next Time BackUp : ${today.getFullYear()}/${padStart(month)}/${padStart(date)}  ${padStart(hour+1)}:${padStart(min)}\n`);
+        await console.log(`BackUP Done(${time}.sql)\n`)
+        await console.log(`Next Time BackUp : ${today.getFullYear()}/${padStart(month)}/${padStart(date)}  ${padStart(hour+1)}:${padStart(min)}\n`);
 }
 function padStart(num){
     return num < 10 ? '0' + num : num
